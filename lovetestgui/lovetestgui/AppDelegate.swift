657d8ef2d28e5d3bdc303d06a7a041ca7543f183
Francesco Mattiussi <f.mattiussi2004@gmail.com>
2020-02-16 17:15:56 +0100

    aggiunta la gui

---
+++ lovetestgui/lovetestgui/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  lovetestgui
+//
+//  Created by Francesco Mattiussi on 16/02/2020.
+//  Copyright © 2020 Francesco Mattiussi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
